e62f538f84c448359b379b71e76672e7aacc8885
Timur Gain <timur.gain@gmail.com>
2023-05-01 00:41:20 +0400

    fix: isAuthenticated moddleware

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,23 +2,17 @@ const jsonwebtoken = require('jsonwebtoken');
 const config = require('../config');
 const { AuthenticationRequiredError } = require('../errors/castomErrors');
 
-function readCookieCredentials(req, res, next) {
+function isAuthenticated(req, res, next) {
   const { jwt } = req.cookies;
-  if (!jwt) return next();
+  if (!jwt) return next(new AuthenticationRequiredError());
   let payload;
   try {
     payload = jsonwebtoken.verify(jwt, config.jwt.secretKey);
   } catch {
-    return next();
+    return next(new AuthenticationRequiredError());
   }
   req.user = payload;
   return next();
 }
 
-function isAuthorized(req, res, next) {
-  // it checks if there is user id which is taken from jwt provided with httpOnly cookie
-  if (!req.user || !req.user._id) next(new AuthenticationRequiredError());
-  next();
-}
-
-module.exports = { readCookieCredentials, isAuthorized };
+module.exports = { isAuthenticated };
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,25 +3,18 @@ const jsonParser = require('express').json();
 const routerUsers = require('./users');
 const routerCards = require('./cards');
 const { login, createUser, logout } = require('../controllers/users');
-const { readCookieCredentials, isAuthorized } = require('../middlewares/auth');
+const { isAuthenticated } = require('../middlewares/auth');
 const { signupValidation, signinValidation } = require('../middlewares/validation/user');
 const { UrlNotFoundError } = require('../errors/castomErrors');
-
-// crash-test (need to pass a code review)
-router.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
 
 // registration, login, logout
 router.post('/signup', jsonParser, signupValidation, createUser);
 router.post('/signin', jsonParser, signinValidation, login);
-router.post('/logout', readCookieCredentials, isAuthorized, logout);
+router.post('/logout', isAuthenticated, logout);
 
 // main app routes, required to be authenticated
-router.use('/users', readCookieCredentials, isAuthorized, routerUsers);
-router.use('/cards', readCookieCredentials, isAuthorized, routerCards);
+router.use('/users', isAuthenticated, routerUsers);
+router.use('/cards', isAuthenticated, routerCards);
 
 // 404, url not found
 router.use('*', (req, res, next) => next(new UrlNotFoundError()));
